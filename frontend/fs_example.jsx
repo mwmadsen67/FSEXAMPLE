97e2f8b0a3871f615856c0596c93c42c1f5684f4
Mike Madsen <mikewmadsen@gmail.com>
2021-07-30 14:25:01 -0700

    Add user auth comment

--- frontend/fs_example.jsx
+++ frontend/fs_example.jsx
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom';
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   // const store = configureStore();
+  // starting user auth now
 
   ReactDOM.render(<h1>React is working</h1>, root);
 });
